470105aa96eb0e5e2f92bb5fbf2ca105fb01b1b3
Kalaiyarasan06 <130954271+Kalaiyarasan06@users.noreply.github.com>
2025-02-09 12:32:15 +0530

    folder structure added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages,setup
+
+setup(
+  name='AI Medical Bot',
+  version='0.0.0',
+  author='kalaiyarasan',
+  author_email='vkalaiyarasan978@gmail.com',
+  packages=find_packages(),
+  install_requires=[]
+)
